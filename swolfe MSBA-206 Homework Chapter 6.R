fac7cf4c8cb69589d3273cee8f920f80922b3aa4
Samuel Wolfe <wolfesamk@gmail.com>
2023-07-08 09:48:07 -0700

    finished 6.3 regressions using R. Turning this in.
    Signed-off-by: Samuel Wolfe <wolfesamk@gmail.com>

--- swolfe MSBA-206 Homework Chapter 6.R
+++ swolfe MSBA-206 Homework Chapter 6.R
@@ -1,19 +1,14 @@
 #Samuel Wolfe
 #Homework Chapter 6 in R
-#July 7th, 2023
+#July 8th, 2023
 #installed packages
 #ggpubr
-library(dplyr) #3
+library(dplyr) #4
 library(data.table) #1
 library(curl) #2
-library(fastDummies) #5
-#library(ISLR2)
-library(leaps) #6
-library(lessR) #4
-#library(linprog)
-#library(lpSolve)
-options(rstudio.help.showDataPreview = FALSE)
-getOption("rstudio.help.showDataPreview")
+library(fastDummies) #3
+library(glmnet) #6
+library(leaps) #5
 
 #Question 6.3
 #Preparing the data
@@ -22,52 +17,44 @@ dfAirBase <- fread("https://raw.githubusercontent.com/wolfesamk/MSBA-206/main/dm
 #verifying dataframe shape and columns
 names(dfAirBase)
 dim(dfAirBase)
-#seperating FARE for readds
-fares <- dfAirBase$FARE
-#setting target variable
-targetVar <- 'FARE'
-
-#6.3.a.
-#creating dataframe without categorical variables
-dfAirInt <- dfAirBase%>% select(where(is.numeric))
-airIntCol <- names(dfAirInt) #using later for 6.3.b.
-cor_mtx <- cor(dfAirInt)
-round(cor_mtx[, 10], 3)
-#here we see that Distance has the greatest correlation with Fare.
-#followed by Coupon.
-
-#6.3.b.
-#removing first four columns
-dfAirAllCat <- dfAirBase[,-1:-4]
-names(dfAirAllCat)
-dim(dfAirAllCat)
-#removing non cat columns
-dfAirCat <- select(dfAirAllCat, -airIntCol)
-airCatCol <- c(names(dfAirCat))
-#readding FARE
-dfAirCat$FARE <-fares
-#producing pivot tables using lessR
-meanVACATION <- pivot(data=dfAirCat, compute=mean, variable=FARE, by=c(VACATION))
-meanSW <- pivot(data=dfAirCat, compute=mean, variable=FARE, by=c(SW))
-meanGATE <- pivot(data=dfAirCat, compute=mean, variable=FARE, by=c(GATE))
-meanSLOT <- pivot(data=dfAirCat, compute=mean, variable=FARE, by=c(SLOT))
-#printing tables
-meanVACATION
-meanSW
-meanGATE
-meanSLOT
-#The best categorical variable for predicting FARE seems to be SW. Followed by Vacation.
-
-#6.3.c.i
 #creating dummy variables
+dfAirAllCat <- dfAirBase[,-1:-4]
+#adding dummies
 dfAirDummy <- dummy_cols(dfAirAllCat, remove_first_dummy = TRUE, remove_selected_columns = TRUE)
+#moving FARE to end
+dfAirDummy <- dfAirDummy %>% select(-FARE,FARE)
+#creating X and Y
+airX <-model.matrix(FARE ~., data = dfAirDummy)[,-1]
+airY <-dfAirDummy$FARE
+#air.colnames(y) <- c("FARE")
+#dfAir3 <-cbind(airY, airX)
+
 #splitting the data
 set.seed(1)
-airTrain.i <- sample(c(TRUE, FALSE),
-                   nrow(dfAirDummy),
-                   replace = TRUE)
-airTest.i <-(!airTrain.i)
-#6.3.c.ii
+airTrain.i <- sample(1:nrow(airX),nrow(airX)*0.6)
+airTest.i <- (-airTrain.i)
+airTest.i.y <- airY[airTest.i]
+
+#6.3 Best Subset Selection
+air.i.best <-regsubsets(FARE ~., data = dfAirDummy[airTrain.i, ], nvmax = 13)
+air.i.best.test.mat <-model.matrix(FARE ~ ., data=dfAirDummy[airTest.i, ])
+air.i.val.errors <- rep(NA, 13)
+for (i in 1:13) {
+  coefi <- coef(air.i.best, id = i)
+  pred <- air.i.best.test.mat[, names(coefi)] %*% coefi
+  air.i.val.errors[i] <- mean((dfAirDummy$FARE[airTest.i]-pred)^2)
+}
+summary(air.i.best)
+air.i.best.row <- which.min(air.i.val.errors)
+air.i.best.row
+air.i.val.errors[air.i.best.row]
+coef(air.i.best, air.i.best.row)
+alldummynames <- names(dfAirDummy[,-1:-2])
+air.i.best.pred <- alldummynames[-12]
+#these are the best predictors.
+air.i.best.pred
+
+#6.3 Forward Stepwise
 air.ii.step.best.fwd <-regsubsets(FARE ~., data = dfAirDummy[airTrain.i, ], nvmax = 14, method = "forward")
 air.ii.step.test.mat <-model.matrix(FARE ~ ., data=dfAirDummy[airTest.i, ])
 air.ii.val.errors <- rep(NA, 13)
@@ -76,6 +63,64 @@ for (i in 1:13) {
   pred <- air.ii.step.test.mat[, names(coefi)] %*% coefi
   air.ii.val.errors[i] <- mean((dfAirDummy$FARE[airTest.i]-pred)^2)
 }
-air.ii.val.errors
-summary(air.step.full.fwd)
-air.step.full.fwd
+summary(air.ii.step.best.fwd)
+air.ii.best.row <- which.min(air.ii.val.errors)
+air.ii.best.row
+air.ii.val.errors[air.ii.best.row]
+coef(air.ii.step.best.fwd, air.ii.best.row)
+alldummynames <- names(dfAirDummy[,-1:-2])
+alldummynames <- alldummynames[-2]
+air.ii.best.pred <- alldummynames[-11]
+#these are the best predictors.
+air.ii.best.pred
+
+#6.3 Backward Stepwise
+air.iii.step.best.bwd <-regsubsets(FARE ~., data = dfAirDummy[airTrain.i, ], nvmax = 14, method = "backward")
+air.iii.step.test.mat <-model.matrix(FARE ~ ., data=dfAirDummy[airTest.i, ])
+air.iii.val.errors <- rep(NA, 13)
+for (i in 1:13) {
+  coefi <- coef(air.iii.step.best.bwd, id = i)
+  pred <- air.iii.step.test.mat[, names(coefi)] %*% coefi
+  air.iii.val.errors[i] <- mean((dfAirDummy$FARE[airTest.i]-pred)^2)
+}
+summary(air.iii.step.best.bwd)
+air.iii.best.row <- which.min(air.iii.val.errors)
+air.iii.best.row
+air.iii.val.errors[air.iii.best.row]
+coef(air.iii.step.best.bwd, air.iii.best.row)
+alldummynames <- names(dfAirDummy[,-1:-2])
+alldummynames <- alldummynames[-2:-3]
+air.iii.best.pred <- alldummynames[-8:-10]
+#these are the best predictors.
+air.iii.best.pred
+
+#6.3 Ridge
+#setting up ridge
+grid <- 10^seq(10, -2, length = 100)
+ridge.mod <- glmnet(airX, airY, alpha = 0, lambda = grid)
+#finding best lambda
+cv.out <- cv.glmnet(airX[airTrain.i, ], airY[airTrain.i], alpha = 0)
+plot(cv.out)
+bestlam <- cv.out$lambda.min
+bestlam
+#running with best lambda
+ridge.pred <- predict(ridge.mod, s = bestlam,
+                      newx = airX[airTest.i, ])
+mean((ridge.pred - airTest.i.y)^2)
+out <- glmnet(airX, airY, alpha = 0)
+predict(out, type = "coefficients", s = bestlam)[1:14, ]
+
+#6.3 Lasso
+lasso.mod <- glmnet(airX[airTrain.i, ], airY[airTrain.i], alpha = 1,lambda = grid)
+plot(lasso.mod)
+cv.out <- cv.glmnet(airX[airTrain.i, ], airY[airTrain.i], alpha = 1)
+plot(cv.out)
+bestlam <- cv.out$lambda.min
+lasso.pred <- predict(lasso.mod, s = bestlam,
+                      newx = airX[airTest.i, ])
+mean((lasso.pred - airTest.i.y)^2)
+out <- glmnet(airX, airY, alpha = 1, lambda = grid)
+lasso.coef <- predict(out, type = "coefficients",
+                      s = bestlam)[1:14, ]
+lasso.coef[lasso.coef != 0]
+
